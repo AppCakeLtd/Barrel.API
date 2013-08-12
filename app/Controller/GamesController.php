0be36a14a3f8425ed5f94e79691c8b232ae2da00
Thanos Siopoudis <thanoulas@gmail.com>
2013-08-12 09:46:37 +0100

    [GamesController]: Refactor the controller to search and return data for a game and add a method to insert a game in the database

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -25,33 +25,141 @@
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
  
- class GamesController extends AppController {
-	 public $components = array('RequestHandler');
+class GamesController extends AppController {
+	public $components = array('RequestHandler', 'CrossController');
+	public $uses = array('Game', 'WineBuild');
 	 
-	 public function index() {
+	public function index() {
 		 
-	 }
+	}
 	 
-	 public function getAllGames() {
-		 $this->set('allGames', $this->Game->find('all'));
-		 $this->set('_serialize', array('allGames'));
-	 }
+	public function getAllGames() {
+	   $this->set('allGames', $this->Game->find('all'));
+	   $this->set('_serialize', array('allGames'));
+	}
 	 
-	 public function searchForGame() {
-		 if ($this->request->is('get')) {
-		 	$gameToSearchFor = $this->request->query["gameName"];
+	public function searchForGame() {
+		if ($this->request->is('get')) {
+		    $view = new View($this);
+		    $html = $view->loadHelper('Html');
+		    
+    		$gameIdentifiers = $this->request->query["identifier"];
+    		$gameName = $this->request->query["gameName"];
 		 	
-		 	$conditions = array(
-		 		'conditions' => array(
-			 		'or' => array(
-			 			"Game.identifiers LIKE" => "%$gameToSearchFor%"
-			 		)
-			 	)
-		 	);
-		 
-			$this->set('results', $this->Game->find('all', $conditions));
-			
-			$this->set('_serialize', array('results'));
-		 }
-	 }
- }
+    		$conditions = array(
+		        'conditions' => array(
+		            'or' => array(
+		                 "Game.identifiers LIKE" => "%$gameIdentifiers%",
+		                 "Game.name LIKE" => "%$gameName%"
+		             )  
+		         )
+		    );
+		    
+		    $gamesFound = $this->Game->find('all', $conditions);
+		    // Convert paths to URLs
+		    for($i = 0; $i < sizeof($gamesFound); $i++) {
+    		    $gamesFound[$i]["Game"]["coverArtURL"] = $html->url($gamesFound[0]["Game"]["coverArtURL"], true);
+    		    $gamesFound[$i]["Game"]["recipeURL"] = $html->url($gamesFound[0]["Game"]["recipeURL"], true);
+    		    
+    		    $portEngineData = $this->CrossController->getWineBuildByID($gamesFound[$i]["Game"]["wineBuildID"], $this->WineBuild);
+    		    $portEngineURL = "http://api.appcake.co.uk" . $portEngineData[0]["WineBuild"]["archivePath"];
+    		    
+    		    // Get the engine URL and add it to the results
+    		    $gamesFound[$i]["Game"]["engineURL"] = $portEngineURL;
+		    }
+		    
+
+		    $this->set('results', $gamesFound);
+		    $this->set('_serialize', array('results'));
+	    }
+    }
+	 
+	public function addNewGame() {
+	    // debug($this->request);
+    	if ($this->request->is('post')) {
+        	if (!empty($this->request->params['form']['game']['tmp_name']) && is_uploaded_file($this->request->params['form']['game']['tmp_name'])) {
+        	    $filename = basename($this->request->params['form']['game']['name']); 
+                move_uploaded_file(
+                    $this->request->params['form']['game']['tmp_name'],
+                    WWW_ROOT . 'recipes' . DS . $filename
+                );
+        	
+            	$gameData["name"] = $this->request->data["name"];
+            	$gameData["identifiers"] = $this->request->data["identifiers"];
+            	$gameData["wineBuildID"] = $this->request->data["wineBuildID"];
+            	$gameData["userID"] = $this->request->data["userID"];
+            	$gameData["recipeURL"] = $this->webroot . 'recipes' . DS . $filename;
+
+            	if ($this->Game->save($gameData)) {
+                	$response = new GameGenericResponse();
+                	$response->gameID = $this->Game->id;
+                	$response->responseCode = 200;
+                	$response->responseDescription = "OK";
+                }
+                else {
+                    $response = new GameGenericResponse();
+                    $response->gameID = 0;
+                    $response->responseCode = 500;
+                    $response->responseDescription = "Error saving in the Database!";
+                }
+            }
+            else {
+                $response = new GameGenericResponse();
+                $response->gameID = 0;
+                $response->responseCode = 501;
+                $response->responseDescription = "No file data was sent to the server!";
+            }
+        }
+        else {
+            $response = new GameGenericResponse();
+            $response->gameID = 0;
+            $response->responseCode = 504;
+            $response->responseDescription = "No data was sent to the server!";
+        }
+
+        $this->set('results', $response);
+        $this->set('_serialize', array('results'));
+    }
+    
+    public function updateGameArtwork() {
+        if ($this->request->is('post')) {
+            if (!empty($this->request->params['form']['coverArtURL']['tmp_name']) && is_uploaded_file($this->request->params['form']['coverArtURL']['tmp_name'])) {
+        	    $filename = basename($this->request->params['form']['coverArtURL']['name']); 
+                move_uploaded_file(
+                    $this->request->params['form']['coverArtURL']['tmp_name'],
+                    WWW_ROOT . 'artwork' . DS . $filename
+                );
+                
+                $gameData["id"] = $this->request->data["id"];
+                $gameData["coverArtURL"] = $this->webroot . 'artwork' . DS . $filename;
+                if ($this->Game->save($gameData)) {
+                    $response = new GameGenericResponse();
+                	$response->gameID = $this->Game->id;
+                	$response->responseCode = 200;
+                	$response->responseDescription = "OK";
+                }
+                else {
+                    $response = new GameGenericResponse();
+                    $response->gameID = 0;
+                    $response->responseCode = 500;
+                    $response->responseDescription = "Error saving in the Database!";
+                }
+            }
+        }
+        else {
+            $response = new GameGenericResponse();
+            $response->gameID = 0;
+            $response->responseCode = 504;
+            $response->responseDescription = "No data was sent to the server!";
+        }
+
+        $this->set('results', $response);
+        $this->set('_serialize', array('results'));
+    }
+}
+ 
+final class GameGenericResponse {
+    public $gameID;
+    public $responseCode;
+    public $responseDescription;
+}
