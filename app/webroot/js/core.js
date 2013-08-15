5f6805d0e8bc5486e7972812e85b4e77540cb50e
Thanos Siopoudis <thanoulas@gmail.com>
2013-08-15 01:11:50 +0100

    [Core.js]: Remove unneeded code transferred over from the main website

--- app/webroot/js/core.js
+++ app/webroot/js/core.js
@@ -18,33 +18,7 @@
 		},
 		
 		attachEvents: function() {
-			var base = this;
 			
-			base.$navbar.find('ul.nav a:lt(2)').on({
-				click: function(e) {
-					var $el = $(e.currentTarget);
-					e.preventDefault();
-					
-					var scrollEl = $el.attr('href') == '#' ? '#home' : $el.attr('href');
-					var offset = $el.attr('href') == '#' ? 60 : 0;
-					
-					base.scrollTo(scrollEl, 500, offset);
-				}
-			});
-			
-			$(window).on({
-				scroll: function(e) {
-					var topScroll = $('html').scrollTop() > 0 ? $('html').scrollTop() : $('body').scrollTop();
-					if (topScroll < $('#features').offset().top) {
-						base.$navbar.find('ul.nav li').removeClass('active');
-						base.$navbar.find('ul.nav li:first').addClass('active');
-					}
-					else if (topScroll >= $('#features').offset().top) {
-						base.$navbar.find('ul.nav li').removeClass('active');
-						base.$navbar.find('ul.nav li:eq(1)').addClass('active');
-					}
-				}
-			});
 		},
 		
 		scrollTo: function(el, speed, offset) {
