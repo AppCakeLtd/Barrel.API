fedd9b934ec6b459062aac52bf7afe30ad5a48f7
Thanos Siopoudis <thanoulas@gmail.com>
2013-08-28 17:26:29 +0100

    [Core.js]: Show active buttons by adding a class

--- app/webroot/js/core.js
+++ app/webroot/js/core.js
@@ -30,6 +30,21 @@
             var base = this;
 		
     		base.$content.find('.thumbnail > p').ellipsis();
+    		base.prepareActiveButtons();
+		},
+		
+		prepareActiveButtons: function() {
+			var base = this,
+				components = window.location.href.split('/'),
+				lastPathComponent = '';
+			
+			if (components && components.length) {
+				lastPathComponent = components[components.length - 1];
+			}
+			
+			// Highlight the active button
+			base.$content.find('.btn-toolbar a.btn').removeClass('active');
+			base.$content.find('.btn-toolbar a.btn[data-letter="' + lastPathComponent + '"]').addClass('active');
 		},
 		
 		scrollTo: function(el, speed, offset) {
