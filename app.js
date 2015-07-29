53877596618bb93c6fbb1c58142e17d49abe53a6
Thanos Siopoudis <siopoudis@me.com>
2015-07-29 13:32:15 +0100

    Add mongoose and engine routes

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
+// MARK: Includes
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var db = require('mongoose');
 
+// MARK: Routes
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var engines = require('./routes/engines');
+var githooks = require('./routes/githooks');
 
 var app = express();
+require('./configuration/configuration')(app);
+db.connect(app.get('dbURL'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,18 +24,22 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/engines', engines);
+app.use('/githooks', githooks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,23 +47,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) { //eslint-disable-line no-unused-vars
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function(err, req, res, next) { //eslint-disable-line no-unused-vars
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
